82db2abfa305f17ce0150b44b5d376c4db35acdb
Tejomay Padole <tejomaypadole128@gmail.com>
2019-10-28 22:01:04 +0530

    Update headerfile.c

--- headerfile.c
+++ headerfile.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+void initialize(struct student s[],int size)
+{
+	int i;
+	for(i=0;i<size;i++)
+	{
+		s[i].id=s[i].year=s[i].hostel_no=s[i].floor_no=s[i].room_no=s[i].id_roomate=-1;
+		s[i].name='\0';
+		s[i].dept='\0';
+	}	
+}
