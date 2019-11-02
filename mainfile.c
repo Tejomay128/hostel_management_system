d5192a87e79507b143357f5607fc6012b88d09eb
Tejomay Padole <tejomaypadole128@gmail.com>
2019-11-02 15:31:29 +0530

    Update mainfile.c

--- mainfile.c
+++ mainfile.c
@@ -7,7 +7,7 @@ int main()
 	struct student old_stud[N], new_stud[N];
 	struct room room[K/2];
 	int choice,sh_c,ns_c,os_c,dh_c, old_sz;
-	int f1, f2, f3, f4, i, floor_pref_count, roommate_pref_count;
+	int f1, f2, f3, f4, i,j, floor_pref_count, roommate_pref_count;
 	f2=f3=f4=0;
 	char repeat='\0';
 	initialize_s(old_stud, N);
@@ -16,11 +16,13 @@ int main()
 	initialize_r(room,K/8,2);
 	initialize_r(room,K/8,3);
 	initialize_r(room,K/8,4);
+		
 	sh_c=ns_c=os_c=dh_c=0;    // sh_c = students who want same hostel,ns_c = new students,os_c = seniors living outside,dh_c = students from different hostel
 	old_sz=assign(old_stud, new_stud, N, &sh_c, &ns_c, &os_c, &dh_c);
+	
 	f1=new_stud_allocate(new_stud, room, ns_c, K/2);
 	old_stud_allocate(old_stud, room, old_sz, K/2, &f2, &f3, &f4);
-
+	
 	floor_pref_count=roommate_pref_count=0;
 	for(i=0;i<old_sz;i++)
 	{
@@ -50,7 +52,7 @@ int main()
 			case 3 :    printf("\nNo.of students who got rooms as per their roommate preference: %d",roommate_pref_count);
 						break;
 					
-			case 4 :	printf("\nNo. of seats vacant in each floor: 1-> %d\n2-> %d\n3-> %d4-> %d",f1, f2, f3, f4);
+			case 4 :	printf("\nNo. of seats vacant in each floor:\n1-> %d\n2-> %d\n3-> %d\n4-> %d",f1, f2, f3, f4);
 						break;	
 
 			default : 	printf("\nWrong choice.");		
