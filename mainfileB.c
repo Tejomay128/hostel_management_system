a6a2809c39a285035cf2e0fecf9a231fc5a04e73
Tejomay Padole <tejomaypadole128@gmail.com>
2019-11-04 10:34:28 +0000

    Update mainfileB.c

--- mainfileB.c
+++ mainfileB.c
@@ -1,4 +1,4 @@
-#include"headerfile.h"
+#include "headerfile.h"
 
 int main()
 {
@@ -15,5 +15,31 @@ int main()
 	stable_combination(stud, N-size);
   	printf("Based on the student data, the stable combinations are:\n");
   	assign_rooms(stud, room, N-size, K);
+  													//Que C
+  	int tick[K/2];                              
+  	int i,j,l,rooms_per_floor;
+  	for(i=0;i<K/2;i++)
+  	{
+  		tick[i]=0;
+  	} 
+  	i=0;
+  	rooms_per_floor=K/8;
+  	while(i<N_size)
+  	{
+  		j=find_index(stud,size,stud[i].roommate_id);
+  		if(tick[i] == 0)
+  		{
+  			printf("\nPossible seat allotments(room nos.) for students with IDs %d and %d are:\n",stud[i].id,stud[j].id)
+  			if(stud[i].p.floor_pref == stud[j].p.floor_pref)
+  			{
+  				for(l=1;l<=rooms_per_floor;l++)
+  				{
+  					printf("%d\t",1000*stud[i].p.floor_pref + l);
+  					
+  				}
+  			}
+  		}
+  		i++;
+  	}
 	return 0;
 }
